e6f5c4dd9e423f5235cf18986b94d91aa320b8e2
thiago23223 <lucenatato2006@gmail.com>
2025-03-22 16:03:46 -0300

    añado un archivo, donde tengo todos los modulos q voy a implementar y otro archivo donde se ejecutara el programa principal

---
+++ teoriaClase2/main.py
@@ -0,0 +1,13 @@
+#diccionario de las peliculas argentinas candidatas a mejor pelicula extranjera
+
+peliculas = {
+    'La tregua': [1975, 'Nominada', 108],
+    'Camila': [1986, 'Nominada', 105],
+    'La historia oficial': [1986, 'Ganadora', 112],
+    'Tango, no me dejes nunca': [1999, 'Nominada', 115],
+    'El hijo de la novia': [2002, 'Nominada', 123],
+    'El secreto de sus ojos': [2010, 'Ganadora', 129],
+    'Relatos salvajes': [2015, 'Nominada', 122],
+    'Argentina, 1985': [2023, 'Nominada', 140]
+}
+
---
+++ teoriaClase2/peliculas.py
@@ -0,0 +1,2 @@
+def input_movies():
+    "" """" esta funcion me devuelve una lista con la duracion de las peliculas ingresadas por teclado """"""
